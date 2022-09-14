54878608b389a3de500eae6153823f55f0b71f7d
KevinTopollaj <kevintopollaj@gmail.com>
2022-09-14 21:45:29 +0200

    update the snapshot to change done button

--- Today/Today/ListViewController/ReminderListViewController+DataSource.swift
+++ Today/Today/ListViewController/ReminderListViewController+DataSource.swift
@@ -13,6 +13,17 @@ extension ReminderListViewController {
   //  A snapshot represents the state of your data at a specific point in time.
   typealias Snapshot = NSDiffableDataSourceSnapshot<Int, Reminder.ID>
   
+  func updateSnapshot(reloading ids: [Reminder.ID] = []) {
+    // initialize snapshot and apply it to datasource
+    var snapshot = Snapshot()
+    snapshot.appendSections([0])
+    snapshot.appendItems(reminders.map { $0.id })
+    if !ids.isEmpty {
+      snapshot.reloadItems(ids)
+    }
+    dataSource.apply(snapshot)
+  }
+  
   func cellRegistrationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, id: Reminder.ID) {
     let reminder = reminder(for: id)
     
@@ -35,6 +46,7 @@ extension ReminderListViewController {
     var reminder = reminder(for: id)
     reminder.isComplete.toggle()
     update(reminder, with: id)
+    updateSnapshot(reloading: [id])
   }
   
   private func doneButtonConfiguration(for reminder: Reminder) -> UICellAccessory.CustomViewConfiguration {
