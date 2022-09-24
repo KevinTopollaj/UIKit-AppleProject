686f700caa5ba1eaef99b42aa41e3839caf2ad8e
KevinTopollaj <kevintopollaj@gmail.com>
2022-09-24 19:05:39 +0200

    Read all reminders

--- Today/Today/Models/ReminderStore.swift
+++ Today/Today/Models/ReminderStore.swift
@@ -18,4 +18,25 @@ class ReminderStore {
   var isAvailable: Bool {
     EKEventStore.authorizationStatus(for: .reminder) == .authorized
   }
+  
+  // MARK: - Helper Methods -
+  
+  func readAll() async throws -> [Reminder] {
+    guard isAvailable else { throw TodayError.accessDenied }
+    
+    let predicate = ekStore.predicateForReminders(in: nil)
+    let ekReminders = try await ekStore.fetchReminders(matching: predicate)
+    
+    // The resulting array contains only the reminders that have alarms corresponding to their due dates.
+    let reminders: [Reminder] = ekReminders.compactMap { ekReminder in
+      do {
+        return try Reminder(with: ekReminder)
+      } catch TodayError.reminderHasNoDueDate {
+        return nil
+      }
+    }
+    
+    return reminders
+    
+  }
 }
